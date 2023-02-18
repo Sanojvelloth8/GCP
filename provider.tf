373422591608c53e86fc574f0ff0483f6bf5dd9f
Sanoj MA <sanojma@Sanojs-MacBook-Pro.local>
2023-02-18 14:24:38 +0530

    eight

--- provider.tf
+++ provider.tf
@@ -14,7 +14,7 @@ terraform {
 provider "aws" {
   profile = "default"
   region  = "ap-south-1"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = "${var.key}"
+  secret_key = "${var.value}"
 }
 
