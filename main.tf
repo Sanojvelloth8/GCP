663820496903cd7b39725285818b8936fa167696
Sanoj MA <sanojma@Sanojs-MacBook-Pro.local>
2023-02-18 13:57:12 +0530

    second commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+#Resource to create dynamodb table 
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "Employee"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "EmployeeId"
+
+  attribute {
+    name = "EmployeeId"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Dynamo-DB-Table"
+    Environment = "Dev"
+  }
+}
