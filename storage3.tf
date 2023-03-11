da7933bba66eddec9905bbc4ba02dbfc8e597f08
Sanoj MA <sanojma@Sanojs-MacBook-Pro.local>
2023-03-11 21:58:18 +0530

    22

---
+++ storage3.tf
@@ -0,0 +1,37 @@
+provider "google" {
+  project     = "infrastructure-testing-project"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+  
+}
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "rehnasanoj8888"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+
+
+
+
+
+
